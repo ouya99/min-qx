939b509a0b7352e4c0b95da65d70f2bbf6e51a35
sidalikharef <sidalikharef@gmail.com>
2024-12-06 01:28:46 +0100

    PUSH

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173, // Default Vite port
-  },
+    base: "./", // Ensures relative paths for assets in production
+    build: {
+        outDir: "dist", // Match your Electron build configuration
+    },
+    plugins: [react()],
+    server: {
+        port: 5173, // Default Vite port
+    },
 });
-
